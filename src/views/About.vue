4556cdce258395736587b819868c12aae835e8d9
kwakchaewon <ksh03003@naver.com>
2025-04-10 12:52:33 +0900

    [chore] init commit
    1. vue3, ts 기반 프로젝트 생성
    2. 의존성 설치: Vue Router, Pinia, VueUse, Axios
    3. 기본 컴포넌트 구현 및 라우팅 설정

---
+++ src/views/About.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="about">
+    <h1>About</h1>
+    <p>This is the about page of your application.</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'AboutView'
+})
+</script>
+
+<style scoped>
+.about {
+  padding: 2rem;
+  text-align: center;
+}
+</style> 
---
+++ src/views/Home.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="home">
+    <h1>Welcome to Vue 3 + TypeScript</h1>
+    <p>This is the home page of your application.</p>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  name: 'HomeView'
+})
+</script>
+
+<style scoped>
+.home {
+  padding: 2rem;
+  text-align: center;
+}
+</style> 
