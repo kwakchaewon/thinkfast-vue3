8e97dcb39043c5654aa0928f5d4719b5a41cb3f0
kwakchaewon <ksh03003@naver.com>
2025-04-18 09:38:42 +0900

    [chore] 의존성 관련 설정 변경

--- src/main.ts
+++ src/main.ts
@@ -8,7 +8,6 @@ import '@mdi/font/css/materialdesignicons.css'
 import App from './App.vue'
 import router from './router'
 import store from './store'
-import vuetify from './plugins/vuetify'
 import { loadFonts } from './plugins/webfontloader'
 
 import './style.css'
