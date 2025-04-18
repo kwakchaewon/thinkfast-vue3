0aee6b54cf28d4310b3b3fc302e8ac0764960c49
kwakchaewon <ksh03003@naver.com>
2025-04-18 10:13:52 +0900

    [fix] userState 생성 시 localStorage 단에서 가져오도록 변경

--- src/store/index.ts
+++ src/store/index.ts
@@ -8,16 +8,21 @@ interface userState {
   role: string | null
 }
 
+// localStorage에서 사용자 정보를 불러오는 함수
+const loadState = (): userState => {
+  return {
+    username: localStorage.getItem('username'),
+    accessToken: localStorage.getItem('accessToken'),
+    role: localStorage.getItem('role')
+  }
+}
+
 export default createStore<userState>({
-  state: {
-    username: null,
-    accessToken: null,
-    role: null
-  },
+  state: loadState(),
 
   // mutations: state 직접 변경, 변수 증가와 같은 간단 로직
   mutations: {
-    SET_USER_DATA(state : userState, payload: { accessToken: string; username: string; role: string}) {
+    SET_USER_DATA(state: userState, payload: { accessToken: string; username: string; role: string}) {
       const { accessToken, username, role } = payload
       state.accessToken = accessToken
       state.username = username
@@ -29,7 +34,7 @@ export default createStore<userState>({
       localStorage.setItem('role', role);
     },
 
-    CLEAR_USER_DATA (state: userState){
+    CLEAR_USER_DATA(state: userState) {
       state.accessToken = null;
       state.username = null;
       state.role = null;
@@ -49,16 +54,16 @@ export default createStore<userState>({
 
   actions: {
     async logout({ commit }: {commit:Function}) {
-      commit('CLEAR_AUTH')
+      commit('CLEAR_USER_DATA')
       router.push('/login')
     },
 
     async handleTokenExpiration({ commit }: { commit: Function }) {
-      commit('CLEAR_AUTH')
+      commit('CLEAR_USER_DATA')
       router.push('/login')
     },
 
-    async setUser(
+    setUser(
         { commit }: { commit: Function },
         payload: { username: string; accessToken: string; role: string }
     ) {
