21c2e67acf58d31361ef405c6aad885ef6d3faff
kwakchaewon <ksh03003@naver.com>
2025-04-18 10:54:23 +0900

    [feat] 인증 상태에 따라 페이지 리다이렉트 처리 추가
    1. 인증된 사용자 '/', '/login', '/signup'에 접근 시 메인페이지로 리다이렉트
    2. 인증되지 않은 사용자(isAuthenticated = false)가 '/main'에 접근 시 '/'로 리다이렉트
    3. 인증되지 않은 사용자는 '/login'과 '/signup' 정상 접근 가능

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,30 +1,61 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MainView from '@/views/Main.vue'
+import store from '@/store'
+import { useSnackbar } from '@/composables/useSnackbar'
+
+const { showError } = useSnackbar()
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
       name: 'home',
-      component: () => import('../views/Home.vue')
-    },
-    {
-      path: '/about',
-      name: 'about',
-      component: () => import('../views/About.vue')
-    },
-    {
-      path: '/signup',
-      name: 'signup',
-      component: () => import('../views/Signup.vue')
+      component: () => import('@/views/Home.vue'),
+      beforeEnter: (_to, _from, next) => {
+        if (store.getters.isAuthenticated) {
+          next('/main')
+        } else {
+          next()
+        }
+      }
     },
     {
       path: '/main',
       name: 'main',
-      component: MainView,
-      meta: {
-        requiresAuth: true
+      component: () => import('@/views/Main.vue'),
+      beforeEnter: (_to, _from, next) => {
+        if (!store.getters.isAuthenticated) {
+          showError('로그인 세션이 만료됐습니다.')
+          next('/')
+        } else {
+          next()
+        }
+      }
+    },
+    {
+      path: '/login',
+      name: 'login',
+      component: () => import('@/views/Login.vue'),
+      beforeEnter: (_to, _from, next) => {
+        if (store.getters.isAuthenticated) {
+          showError('로그인 세션이 만료됐습니다.')
+          next('/main')
+        } else {
+          next()
+        }
+      }
+    },
+    {
+      path: '/signup',
+      name: 'signup',
+      component: () => import('@/views/Signup.vue'),
+      beforeEnter: (_to, _from, next) => {
+        if (store.getters.isAuthenticated) {
+          showError('로그인 세션이 만료됐습니다.')
+          next('/main')
+        } else {
+          next()
+        }
       }
     }
   ]
