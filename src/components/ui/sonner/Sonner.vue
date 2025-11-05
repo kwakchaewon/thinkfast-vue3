c4a93880ab3ce3accaf8ff03e5db85461d2629ad
kwakchaewon <ksh03003@naver.com>
2025-11-05 22:54:55 +0900

    [chore] 토스트 팝업 테스트 코드 제거

--- src/components/ui/sonner/Sonner.vue
+++ src/components/ui/sonner/Sonner.vue
@@ -1,32 +1,8 @@
 <script lang="ts" setup>
-import { onMounted, nextTick } from 'vue'
 import type { ToasterProps } from "vue-sonner"
 import { Toaster as Sonner } from "vue-sonner"
 
 const props = defineProps<ToasterProps>()
-
-onMounted(async () => {
-  console.log('[Sonner] Toaster component mounted')
-  await nextTick()
-  
-  setTimeout(() => {
-    const toasterElement = document.querySelector('[data-sonner-toaster]')
-    console.log('[Sonner] Toaster element found:', toasterElement)
-    
-    if (toasterElement) {
-      const styles = window.getComputedStyle(toasterElement as Element)
-      console.log('[Sonner] Toaster styles:', {
-        display: styles.display,
-        visibility: styles.visibility,
-        opacity: styles.opacity,
-        zIndex: styles.zIndex,
-        position: styles.position,
-        top: styles.top,
-        right: styles.right,
-      })
-    }
-  }, 100)
-})
 </script>
 
 <template>
