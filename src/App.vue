3a967ac9dd56f53ad26633c0aea3845563293b60
kwakchaewon <ksh03003@naver.com>
2025-11-05 21:28:14 +0900

    [chore] Vuetify → shadcn-vue: 레이아웃 컴포넌트 마이그레이션

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,14 @@
 <template>
-  <v-app>
+  <div class="min-h-screen w-full bg-background text-foreground">
     <router-view></router-view>
-    <v-snackbar
-      v-model="snackbar.show"
-      :color="snackbar.color"
-      :timeout="snackbar.timeout"
-      class="text-center"
-    >
-      {{ snackbar.message }}
-    </v-snackbar>
-  </v-app>
+    <Toaster />
+  </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue'
-import { useSnackbar } from '@/composables/useSnackbar'
-
-export default defineComponent({
-  name: 'App',
-  setup() {
-    const { snackbar } = useSnackbar()
-    return {
-      snackbar
-    }
-  }
-})
+<script lang="ts" setup>
+import { Toaster } from '@/components/ui/sonner'
 </script>
 
-<style>
-html, body {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-  width: 100%;
-}
-
-#app {
-  height: 100%;
-  width: 100%;
-}
-
-.v-snackbar__content {
-  justify-content: center;
-  text-align: center;
-}
+<style scoped>
+/* 기본 스타일은 style.css에서 처리됨 */
 </style> 
