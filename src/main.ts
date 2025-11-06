1b68ab8801983815f3ce87c66dfbc3053e6e29f8
kwakchaewon <ksh03003@naver.com>
2025-11-06 13:31:30 +0900

    [style] Vuetify → shadcn-vue 마이그레이션 작업: Vuetify 플러그인 제거

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,5 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
-import { createVuetify } from 'vuetify'
-import * as components from 'vuetify/components'
-import * as directives from 'vuetify/directives'
-import 'vuetify/styles'
-import '@mdi/font/css/materialdesignicons.css'
 import 'vue-sonner/style.css'
 import App from './App.vue'
 import router from './router'
@@ -13,20 +8,11 @@ import { loadFonts } from './plugins/webfontloader'
 
 import './style.css'
 
-const vuetify = createVuetify({
-  components,
-  directives,
-  theme: {
-    defaultTheme: 'light'
-  }
-})
-
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
 app.use(store)
-app.use(vuetify)
 
 loadFonts()
 
